94d3c5e923d0783bc54a46175f4721c023cfd8dd
PilkeySEK <PilkeySEK@gmx.de>
2025-07-14 15:27:40 +0200

    error message on json parse error

--- core/src/schemas.ts
+++ core/src/schemas.ts
@@ -3,3 +3,7 @@ import * as z from "zod";
 export const StatusMessage = z.object({
     status: z.literal(["connected", "disconnecting", "ping", "pong"])
 });
+
+export const ServerError = z.object({
+    message: z.string()
+});
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,5 @@
 import { WebSocketServer } from "ws";
-import { StatusMessage } from "../../core/src/schemas";
+import { ServerError, StatusMessage } from "../../core/src/schemas";
 
 const wss = new WebSocketServer({ port: 8081 });
 
@@ -8,6 +8,13 @@ wss.on("connection", async (ws) => {
 
     ws.on("message", async (data) => {
         console.log(`Received websocket message: ${data}`);
+        try {
+            const json = JSON.parse(data.toString());
+        } catch (e) {
+            console.error(e);
+            ws.send(JSON.stringify(ServerError.parse({message: "Failed to parse JSON"})));
+            return;
+        }
     });
     ws.send(JSON.stringify(StatusMessage.parse({ status: "connected" })));
 });
