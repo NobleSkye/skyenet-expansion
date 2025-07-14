b692bcc9cb54a124d3565982ad47599e70cb85bd
Anjo2807 <anjodima54@gmail.com>
2025-07-14 15:54:27 +0200

    not so working player controller

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,50 @@
 import 'kaplay'
 import './style.css'
 import kaplay from 'kaplay'
-
+import spaceship from './sprites/spaceship.webp'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
   </div>
 `
 
-kaplay();
+let thrusting = false;
+let speed = 0;
 
+const k = kaplay({
+  buttons: {
+    thrust: {
+      keyboard: ["w"],
+    }
+  }
+});
+
+k.loadSprite("player", spaceship, {
+    sliceX: 1, // how many sprites are in the X axis
+    sliceY: 1, // how many sprites are in the Y axis
+});
+
+const player = k.add([
+  k.sprite("player"),
+  k.pos(100, 100),
+])
+
+k.onButtonDown("thrust", () => {
+  thrusting = true
+});
+ 
+k.onButtonRelease("thrust", () => {
+  thrusting = false;
+})
+
+k.onUpdate("player" ,(player) => {
+  if (thrusting) {
+    if (player.move.speed < 2) {
+      player.move.speed += 0.2
+    }
+  } else {
+    if (player.move.speed > 0) {
+      player.move.speed -= 2
+    }
+  }
+})
