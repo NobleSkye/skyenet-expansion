445b07b54e96eed27829561010421fa233c16796
loglot <prattlog3@gmail.com>
2025-07-16 07:20:14 -0700

    added multi player rendering

--- client/src/lib/gamerenderer.ts
+++ client/src/lib/gamerenderer.ts
@@ -96,38 +96,42 @@ export class GameRenderer {
 
     // Draw player ship using texture atlas
     this.ctx.translate(game.camera.x, game.camera.y);
-    this.ctx.translate(game.player.x, game.player.y);
-    this.ctx.rotate(-((game.player.rotation * Math.PI) / 180));
+    // console.log(game.players)
+    for(let i = 0; i < game.players.length;i++){
+      this.ctx.translate(game.players[i].x, game.players[i].y);
+      this.ctx.rotate(-((game.players[i].rotation * Math.PI) / 180));
 
-    // Apply 3  x scale for player ship
-    this.ctx.scale(3, 3);
+      // Apply 3  x scale for player ship
+      this.ctx.scale(3, 3);
 
-    // Check if atlas is loaded before drawing
-    if (this.atlasManager.areAllLoaded()) {
-      // Choose texture based on engine state and selected ship
-      const shipTexture = game.player.engineActive
-        ? game.player.shipEngineSprite
-        : game.player.shipSprite;
+      // Check if atlas is loaded before drawing
+      if (this.atlasManager.areAllLoaded()) {
+        // Choose texture based on engine state and selected ship
+        const shipTexture = game.players[i].engineActive
+          ? game.players[i].shipEngineSprite
+          : game.players[i].shipSprite;
 
-      this.atlasManager.drawTexture(
-        "entities",
-        shipTexture,
-        this.ctx,
-        -16, // Center the 32x32 sprite
-        -16,
-      );
-    } else {
-      // Fallback: draw a simple rectangle while atlas loads
-      console.log("Atlas not loaded, showing green rectangle");
-      this.ctx.fillStyle = "#00ff00";
-      this.ctx.fillRect(-16, -16, 32, 32);
+        this.atlasManager.drawTexture(
+          "entities",
+          shipTexture,
+          this.ctx,
+          -16, // Center the 32x32 sprite
+          -16,
+        );
+      } else {
+        // Fallback: draw a simple rectangle while atlas loads
+        console.log("Atlas not loaded, showing green rectangle");
+        this.ctx.fillStyle = "#00ff00";
+        this.ctx.fillRect(-16, -16, 32, 32);
+      }
+
+      // Reset scale after drawing
+      this.ctx.scale(0.5, 0.5);
+
+      this.ctx.rotate((game.player.rotation * Math.PI) / 180);
+      this.ctx.translate(-game.player.x, -game.player.y);
+
     }
-
-    // Reset scale after drawing
-    this.ctx.scale(0.5, 0.5);
-
-    this.ctx.rotate((game.player.rotation * Math.PI) / 180);
-    this.ctx.translate(-game.player.x, -game.player.y);
     this.ctx.translate(-game.camera.x, -game.camera.y);
   }
 
