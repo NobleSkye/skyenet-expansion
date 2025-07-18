0341b38994248f5f8c62a9097dcd8c78bc8d3eb3
PilkeySEK <PilkeySEK@gmx.de>
2025-07-18 18:08:10 +0200

    fix pnpm-lock.yaml + add flames to PlayerSchema

--- core/src/Schemas.ts
+++ core/src/Schemas.ts
@@ -6,18 +6,31 @@ function lt(type: WebSocketMessageType) {
 }
 
 const PlayerSchema = z.object({
-      playerID: z.string(),
-      entityID: z.string(),
-      x: z.number().default(0),
-      y: z.number().default(0),
-      velX: z.number().default(0),
-      velY: z.number().default(0),
-      velR: z.number().default(0),
-      rotation: z.number().default(0),
-      engineActive: z.boolean().default(false),
-      shipSprite: z.enum(ShipSprite).default(ShipSprite.Gray),
-      shipEngineSprite: z.enum(ShipEngineSprite).default(ShipEngineSprite.Gray),
-    });
+  playerID: z.string(),
+  entityID: z.string(),
+  x: z.number().default(0),
+  y: z.number().default(0),
+  velX: z.number().default(0),
+  velY: z.number().default(0),
+  velR: z.number().default(0),
+  rotation: z.number().default(0),
+  engineActive: z.boolean().default(false),
+  shipSprite: z.enum(ShipSprite).default(ShipSprite.Gray),
+  shipEngineSprite: z.enum(ShipEngineSprite).default(ShipEngineSprite.Gray),
+  flames: z.optional(
+    z.array(
+      z.object({
+        x: z.number(),
+        y: z.number(),
+        z: z.optional(z.number()),
+        velX: z.optional(z.number()),
+        velY: z.optional(z.number()),
+        size: z.optional(z.number()),
+        rotation: z.optional(z.number()),
+      }),
+    ),
+  ),
+});
 
 export const BaseWebSocketMessageSchema = z.object({
   playerID: z.string(),
@@ -68,8 +81,6 @@ export const PlayerJoinMessageCallback = z.object({
 
 export const UpdatePlayersMessage = z.object({
   type: lt(WebSocketMessageType.UpdatePlayers),
-  playersAdded: z.array(
-    z.object(PlayerSchema),
-  ),
+  playersAdded: z.array(z.object(PlayerSchema)),
   playersRemoved: z.array(z.string()),
 });
